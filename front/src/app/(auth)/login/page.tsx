c50b6fd918f47a552810867cab749a6f04926047
deepanshu verma <deepanshuverma186@gmail.com>
2024-10-25 11:20:37 -0700

    create auth pages

---
+++ front/src/app/(auth)/login/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Label } from '@radix-ui/react-label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+export default function login() {
+  return (
+    <div className=' flex justify-center items-center h-screen' >
+       <div className=' w-[550px] bg-white rounded-xl py-5 shadow-md space-y-2 pl-5 pr-5' >
+       <h1 className=' text-3xl  font-bold text-center' > Clash </h1>
+       <h1 className=' text-2xl  font-bold' > Login </h1>
+       <p>Welcome Back </p>
+       <form>
+          <div className=' mt-4'>
+            <Label htmlFor='email' >Email</Label>
+            <Input id='email' type='email' name='email' placeholder='Enter your email...' />
+          </div>
+          <div className=' mt-4'>
+            <Label htmlFor='password' >Password</Label>
+            <Input id='password' type='password' name='password' placeholder='Enter your password...' />
+          </div>
+          <div className=' text-right font-bold p-2' >
+             <Link href={'forget-password'} >
+             Forget Password ? 
+             </Link>
+          </div>
+          <div className=' mt-4'>
+              <Button className=' w-full' >Submit</Button>
+          </div>
+       </form>
+       <p className=' text-center mt-2' >
+        Don't have an account ? <strong>
+            <Link href={'/register'} > Register </Link>
+        </strong>
+       </p>
+       </div>           
+    </div>
+  )
+}
+
+
+
--- front/src/components/base/HeroSection.tsx
+++ front/src/components/base/HeroSection.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
 
 export default function HeroSection() {
   return (
@@ -7,13 +9,18 @@ export default function HeroSection() {
         <div>
             <Image 
             src='/banner.svg' 
-            width={600} 
-            height={600} 
+            width={500} 
+            height={500} 
             alt='banner' />
         </div>
-        <div>
+        <div className=' text-center space-y-5'>
             <h1 className=' text-6xl  font-bold' > Clash </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, molestiae.</p>
         </div>
+        <Link href={'/login'} >
+        <Button className=' mt-2' > Start free </Button>
+        </Link>
+       
     </div>
   )
 }
