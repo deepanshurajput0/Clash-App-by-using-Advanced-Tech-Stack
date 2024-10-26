835d7428766c45a16b7d9af74e430bc422478c25
deepanshu verma <deepanshuverma186@gmail.com>
2024-10-25 22:32:46 -0700

    setup ejs

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({});
 const app = express();
 const PORT = process.env.PORT || 8080;
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(_dirname, './views'));
 app.get('/', (req, res) => {
-    res.send("<h1> It's working fine </h1>");
+    res.render('welcome');
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,22 @@
 import express,{Application, Request, Response} from 'express'
 import dotenv from 'dotenv'
-
+import path from 'path'
+import { fileURLToPath } from 'url'
+const _dirname = path.dirname(fileURLToPath(import.meta.url))
 dotenv.config({})
 
 const app:Application = express()
 
 const PORT = process.env.PORT || 8080
 
+app.use(express.json())
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(_dirname,'./views'))
+
 app.get('/',(req:Request,res:Response)=>{
-     res.send("<h1> It's working fine </h1>")
+     res.render('welcome');
 })
 
 app.listen(PORT,()=>{
