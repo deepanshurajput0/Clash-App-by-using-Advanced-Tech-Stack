98c426508fda669fab4f6969aab19ded671b819b
deepanshu verma <deepanshuverma186@gmail.com>
2024-11-05 19:46:23 -0800

    get clash by id

--- server/dist/routes/clashRoutes.js
+++ server/dist/routes/clashRoutes.js
@@ -21,6 +21,24 @@ router.get('/', async (req, res) => {
         return res.status(500).json({ message: 'Internal Server Error' });
     }
 });
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const clash = await prisma.clash.findUnique({
+            where: {
+                id: Number(id)
+            },
+        });
+        return res.json({ message: 'Clashed fetched successfully', data: clash });
+    }
+    catch (error) {
+        if (error instanceof ZodError) {
+            const errors = formatError(error);
+            return res.status(422).json({ message: 'Invalid Data', errors });
+        }
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
 router.post('/create', async (req, res) => {
     try {
         const body = req.body;
